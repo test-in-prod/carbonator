2cc9b7f75d53855a74cc404c637cb256889a0150
crypton <crypton@crypton-technologies.net>
2015-02-02 08:30:02 -0800

    Configuration Section update

---
+++ Carbonator/Config/CarbonatorSection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Crypton.Carbonator.Config
+{
+    /// <summary>
+    /// Defines carbonator configuration section
+    /// </summary>
+    public class CarbonatorSection : ConfigurationSection
+    {
+
+        /// <summary>
+        /// Gets current carbonator configuration
+        /// </summary>
+        public static CarbonatorSection Current
+        {
+            get
+            {
+                return ConfigurationManager.GetSection("carbonator") as CarbonatorSection;
+            }
+        }
+
+
+        /// <summary>
+        /// Gets Graphite server settings
+        /// </summary>
+        [ConfigurationProperty("graphite", IsRequired = true)]
+        public GraphiteExportElement Graphite
+        {
+            get { return (GraphiteExportElement)base["graphite"]; }
+            set { base["graphite"] = value; }
+        }
+
+        /// <summary>
+        /// Gets the configured performance counters
+        /// </summary>
+        [ConfigurationProperty("counters", IsRequired = true)]
+        public PerformanceCounterCollection Counters
+        {
+            get { return (PerformanceCounterCollection)base["counters"]; }
+            set { base["counters"] = value; }
+        }
+
+    }
+}
---
+++ Carbonator/Config/GraphiteExportElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Crypton.Carbonator.Config
+{
+    /// <summary>
+    /// Defines configuration properties for exporting data to Graphite
+    /// </summary>
+    public class GraphiteExportElement : ConfigurationElement
+    {
+
+        /// <summary>
+        /// Gets or sets destination Graphite server
+        /// </summary>
+        [ConfigurationProperty("server", IsRequired=true)]
+        public string Server
+        {
+            get { return (string)base["server"]; }
+            set { base["server"] = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets destination Graphite server port (default 2003)
+        /// </summary>
+        [ConfigurationProperty("port", IsRequired = false, DefaultValue = 2003)]
+        public int Port
+        {
+            get { return (int)base["port"]; }
+            set { base["port"] = value; }
+        }
+
+    }
+}
