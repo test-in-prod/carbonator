0c7767f6396ebf91114bd2781457043296036cc9
Omer Levi Hevroni <omerl@soluto.com>
2016-08-03 09:51:27 +0300

    added support for prefix that will be added to each metric. used to support hosted graphite api keys, which sent as the start of the metric

--- Carbonator/Config/GraphiteExportElement.cs
+++ Carbonator/Config/GraphiteExportElement.cs
@@ -32,5 +32,15 @@ namespace Crypton.Carbonator.Config
             set { base["port"] = value; }
         }
 
+        /// <summary>
+        /// Gets or sets destination Graphite server port (default 2003)
+        /// </summary>
+        [ConfigurationProperty("prefix", IsRequired = false, DefaultValue = "")]
+        public string Prefix
+        {
+            get { return (string)base["prefix"]; }
+            set { base["prefix"] = value; }
+        }
+
     }
 }
